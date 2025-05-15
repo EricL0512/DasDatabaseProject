de08e3af86aa038b736e78232a4191bf91e217b7
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2025-05-15 00:40:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+connection = mysql.connector.connect(user='ericl160',
+                                     password='REDACTED',
+                                     host='10.8.37.226',
+                                     database='ericl160_db')
+
+cursor = connection.cursor()
+
+student_id = input("Enter a student_id number: ")
+query = f"CALL Student_Info({student_id})"
+
+cursor.execute(query)
+
+for row in cursor:
+    period, course, room, teacher = [i for i in row]
+    print(f"Period: {period}\nCourse: {course}\nRoom: {room}\nTeacher: {teacher}\n")
+
+cursor.close()
+connection.close()
