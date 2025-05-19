0ef9a4d3cf5a25cffa60203be91f6c5910ccd34f
EricL0512 <144487690+EricL0512@users.noreply.github.com>
2025-05-19 14:30:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import mysql.connector
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
 
-connection = mysql.connector.connect(user='ericl160',
-                                     password='REDACTED',
-                                     host='10.8.37.226',
-                                     database='ericl160_db')
 
-cursor = connection.cursor()
+def get_database_connection() -> PooledMySQLConnection | MySQLConnectionAbstract:
+    connection = mysql.connector.connect(user='ericl160',
+                                         password='222924870',
+                                         host='10.8.37.226',
+                                         database='ericl160_db')
+    return connection
+
+
+def execute_statement(connection: PooledMySQLConnection | MySQLConnectionAbstract, statement):
+    cursor = connection.cursor()
+    cursor.execute(statement)
+    results = []
+
+    for r in cursor:
+        results.append(r)
+
+    cursor.close()
+    connection.close()
+    return results
+
+
+def get_student_schedule(student_id):
+    query = f"CALL Student_Info({student_id})"
+    return execute_statement(get_database_connection(), query)
+
 
 student_id = input("Enter a student_id number: ")
-query = f"CALL Student_Info({student_id})"
+output = get_student_schedule(student_id)
 
-cursor.execute(query)
-
-for row in cursor:
+print()
+for row in output:
     period, course, room, teacher = row[:4]
     print(f"Period: {period}\nCourse: {course}\nRoom: {room}\nTeacher: {teacher}\n")
-
-cursor.close()
-connection.close()
